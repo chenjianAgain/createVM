8d20e4b949d356f94110d6aafbd9379c3831998b
Joseph <Joseph.siyi@gmail.com>
2019-11-27 20:37:34 +0800

    Added env

--- bin/create_vm.ts
+++ bin/create_vm.ts
@@ -3,4 +3,9 @@ import cdk = require('@aws-cdk/core');
 import { CreateVmStack } from '../lib/create_vm-stack';
 
 const app = new cdk.App();
-new CreateVmStack(app, 'CreateVmStack');
+new CreateVmStack(app, 'CreateVmStack', {
+    env: {
+        region: 'us-west-1',
+        account: '374801192098'
+    }
+});

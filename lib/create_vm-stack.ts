075e608f5d0616a8381f4c17629fb5b344f3bcec
Joseph <Joseph.siyi@gmail.com>
2019-11-27 20:09:58 +0800

    Added asg

--- lib/create_vm-stack.ts
+++ lib/create_vm-stack.ts
@@ -1,18 +1,38 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2');
+import autoscaling = require('@aws-cdk/aws-autoscaling'); 
 
 export class CreateVmStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CreateVmQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    // Using default vpc
+    const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
+      isDefault: true
     });
 
-    const topic = new sns.Topic(this, 'CreateVmTopic');
+    // Open port 22 for SSH connection from anywhere
+    const mySecurityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      securityGroupName: "my-test-sg",
+      description: 'Allow ssh access to ec2 instances from anywhere',
+      allowAllOutbound: true 
+    });
+    mySecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.allTraffic(), 'allow public ssh access')
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+
+    // We are using the latest AMAZON LINUX AMI
+    const awsAMI = new ec2.AmazonLinuxImage({generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2});
+    // const linuxAMI = new ec2.GenericLinuxImage()
+    
+    const masterAsg = new autoscaling.AutoScalingGroup(this, 'masterAsg', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: awsAMI,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      associatePublicIpAddress: true,
+      allowAllOutbound: true,
+      keyName: 'devops'
+    }).addSecurityGroup(mySecurityGroup);
   }
 }
